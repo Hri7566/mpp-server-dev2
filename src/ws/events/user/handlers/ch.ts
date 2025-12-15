946dbb8d03a5d1f27df702292ab2e81d51f1421c
Hri7566 <hri7566@gmail.com>
2025-12-14 19:41:50 -0500

    fix channel message

--- src/ws/events/user/handlers/ch.ts
+++ src/ws/events/user/handlers/ch.ts
@@ -6,6 +6,7 @@ export const ch: ServerEventListener<"ch"> = {
         // Switch channel
         if (!socket.rateLimits?.normal.ch.attempt()) return;
         if (typeof msg._id !== "string") return;
+        if (msg._id.length > 512) return;
 
         // So technical and convoluted...
         socket.setChannel(msg._id, msg.set);
